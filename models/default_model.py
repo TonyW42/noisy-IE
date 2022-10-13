0407bef6da65ac617c588d7c8d491bea73915a55
samaritanhu <samaritanhu@gmail.com>
2022-10-13 11:24:00 -0400

    added weighted but exists some bug

--- models/default_model.py
+++ models/default_model.py
@@ -2,21 +2,25 @@ from cmath import log
 import sys 
 sys.path.append("..") 
 sys.path.append("../..") 
-from data import Tokenization, is_aligned, CharacterLevelMapping
 from utils.compute import compute_metrics
 from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer, AutoTokenizer, DataCollatorForTokenClassification
 from datasets import load_dataset
 import argparse
 import torch 
 from evaluate_utils import *
+from torch.utils.tensorboard.writer import SummaryWriter
 import numpy as np
 from utils.log import *
+from models.model import *
+from utils.save_model import current_time
 
 
 class HuggingFaceModel:
     def __init__(self, args):
         self.args = args
         self.log = Log("Hugging Face Model")
+        self.writer = SummaryWriter(os.path.join(self.args.output_dir, current_time())
+    )
 
     def train(self):
         # model = AutoModelForTokenClassification.from_pretrained(self.args.model_name, num_labels=self.args.num_labels)
@@ -24,20 +28,14 @@ class HuggingFaceModel:
         logits_sum = []
         count = 0
         model_prob = dict()
+        data_loader = wnut_multiple_granularity(wnut, self.args)
+        model_main = data_loader.model_dict
+        tokenized_wnut_main = data_loader.data_
         for granularity in self.args.granularities:
-            if granularity == "character":
-                clm = CharacterLevelMapping(self.args.to_char_method)
-                wnut_character_level = clm.character_level_wnut(wnut)
-                tok = Tokenization(self.args.granularities_model[granularity], self.args.prefix_space)
-                tokenized_wnut = tok.tokenize_for_char_manual(wnut_character_level)
-            elif granularity == "subword_50k" or granularity == "subword_30k":
-                self.log.info(self.args.granularities_model[granularity])
-                tok = Tokenization(self.args.granularities_model[granularity], self.args.prefix_space)
-                tokenized_wnut = wnut.map(tok.tokenize_and_align_labels, batched=True) ## was previously wnut_character level 
-            assert is_aligned(tokenized_wnut)
-
             model_name = self.args.granularities_model[granularity]
-            model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=self.args.num_labels)
+            tokenized_wnut = tokenized_wnut_main[model_name]
+
+            model = model_main[model_name]
             tokenizer = AutoTokenizer.from_pretrained(model_name, add_prefix_space=self.args.prefix_space) ## changed here
             data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
                                         
@@ -117,7 +115,6 @@ class HuggingFaceModel:
 
 
             
-            
         # pred = torch.argmax(logits_sum, dim = 1)
         ensumble_f1 = wnut_f1(pred = pred, ref = label)
         print(f"\n The F1-score of the model is {ensumble_f1} \n")
@@ -135,6 +132,68 @@ class HuggingFaceModel:
         # print(f"\n The F1-score of the model is {wnut_f1} \n")
         # print(f"\n The F1-score of the model is {wnut_f1_1} \n")
         # print(f"\n The F1-score of the model is {wnut_f1_2} \n")
+
+    def _train(self):
+        wnut = load_dataset("wnut_17")
+        print(self.args.model_names)
+        data_loader = wnut_multiple_granularity(wnut, self.args)
+        tokenized_wnut_main = data_loader.data_
+        model = weighted_ensemble(data_loader.model_dict, self.args)
+        for granularity in self.args.granularities:
+            model_name = self.args.granularities_model[granularity]
+            tokenized_wnut = tokenized_wnut_main[model_name]
+
+            tokenizer = AutoTokenizer.from_pretrained(model_name, add_prefix_space=self.args.prefix_space) ## changed here
+            data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+            criterion = nn.BCEWithLogitsLoss().to(self.args.device)
+
+            if not self.args.train: 
+                optimizer = None
+                scheduler = None
+            else: 
+                no_weight_decay = lambda param_name: any(
+                    no_decay_name in param_name for no_decay_name in ['LayerNorm', 'layer_norm', 'bias']
+                )
+                optimizer = optim.AdamW(
+                    [
+                        {'params': [param for param_name, param in model.named_parameters() if not no_weight_decay(param_name)]}, 
+                        {'params': [param for param_name, param in model.named_parameters() if no_weight_decay(param_name)], 'weight_decay': 0}
+                    ], 
+                    lr=self.args.lr, 
+                    betas=(0.9, 0.999), 
+                    eps=1E-6, 
+                    weight_decay=0.01
+                )
+                train_steps = self.args.n_epochs * data_loader.__len__
+                warmup_steps = self.args.warmup_proportion * train_steps
+                scheduler = optim.lr_scheduler.LambdaLR(
+                    optimizer, 
+                    lambda global_step: max(
+                        0, 
+                        min(global_step / warmup_steps, 1 - (global_step - warmup_steps) / train_steps)
+                    ) # slanted triangular lr
+                )
+
+            estimator = weighted_estimater( # TODO: change to your estimator!
+                model, 
+                tokenizer, 
+                criterion, 
+                optimizer=optimizer, 
+                scheduler=scheduler, 
+                logger=self.log, 
+                writer=self.writer, 
+                pred_thold=None, 
+                device=self.args.device, 
+                # add other hyperparameters here
+            )
+            print('Running the model...')
+            if self.args.train: 
+                self.logger.info('Training...')
+                estimator.train(self.args, tokenized_wnut['train'], tokenized_wnut['validation'])
+                probs, _ = estimator.test(tokenized_wnut['test'])
+                assert probs.shape[0] == len(tokenized_wnut['test']) 
+            
 
 
 if __name__ == '__main__':
@@ -158,14 +217,26 @@ if __name__ == '__main__':
     parser.add_argument('--train', type=str, default="True")
     parser.add_argument('--device', type=str, default=None)
     parser.add_argument('--ensemble_method', type=str, default="soft")
-    
+
     parser.add_argument('--granularities_model', type=dict, 
                         default= {"character": "google/canine-s",
                                 "subword_50k": "xlm-roberta-base",
                                 "subword_30k" : "bert-base-cased"})
 
+    parser.add_argument('--embed_size_dict', type=dict, 
+                        default= {"google/canine-s": 768,
+                                  "google/canine-c": 768,
+                                  "bert-base-cased" : 768,
+                                  "bert-base-uncased" : 768,
+                                  "xlm-roberta-base" : 1024,
+                                  "roberta-base": 768,
+                                  "roberta-large": 1024,
+                                  "vinai/bertweet-base": 768,
+                                  "cardiffnlp/twitter-roberta-base-sentiment": 768})
+
     args = parser.parse_args()
     args.granularities = args.granularities.split(",")
+    args.model_names = [args.granularities_model[key] for key in args.granularities_model]
 
     model = HuggingFaceModel(args)
-    model.train()
+    model._train()
--- models/model.py
+++ models/model.py
@@ -21,30 +21,33 @@ class wnut_multiple_granularity(Dataset):
         Input: wnut - result of load_dataset("wnut_17")
         """
         self.args = args
-        self.data = defaultdict(DatasetDict)
+        self.data_ = dict()
         self.data_length = defaultdict(int)
-        self.tokenizer_dict = {}
+        tokenizer_dict = {}
+        self.model_dict = dict()
+        print("============== loading models ==============")
         for name in args.model_names:
-            self.tokenizer_dict[name] = AutoTokenizer.from_pretrained(name, add_prefix_space=self.args.prefix_space)
-        # self.tokenizer_dict = tokenizer_dict
+            tokenizer_dict[name] = AutoTokenizer.from_pretrained(name, add_prefix_space=self.args.prefix_space)
+            self.model_dict[name] = transformers.AutoModelForTokenClassification.from_pretrained(name, num_labels=self.args.num_labels)
+        self.tokenizer_dict = tokenizer_dict
 
         ## use previous implementation of tokenization for each granularity
         # TODO
         # Should end up something like 
         # self.data = {model_name : DatasetDict }, where 
         # datadict is created using previous tokenization method in data.py 
-        for name, granularity in zip(self.args.model_names, self.args.granularites):
+        for name, granularity in zip(self.args.model_names, self.args.granularities):
             if granularity == "character":
                 clm = CharacterLevelMapping(self.args.to_char_method)
                 wnut_character_level = clm.character_level_wnut(wnut)
                 tok = Tokenization(self.args.granularities_model[granularity], self.args.prefix_space)
                 tokenized_wnut = tok.tokenize_for_char_manual(wnut_character_level)
-                self.data[name] = tokenized_wnut
+                self.data_[name] = tokenized_wnut
                 self.data_length[name] = tokenized_wnut['train'].num_rows
             elif granularity == "subword_50k" or granularity == "subword_30k":
                 tok = Tokenization(self.args.granularities_model[granularity], self.args.prefix_space)
                 tokenized_wnut = wnut.map(tok.tokenize_and_align_labels, batched=True) ## was previously wnut_character level 
-                self.data[name] = tokenized_wnut
+                self.data_[name] = tokenized_wnut
                 self.data_length[name] = tokenized_wnut['train'].num_rows
             assert is_aligned(tokenized_wnut)
         assert granularity_aligned(self.data_length)
@@ -55,21 +58,22 @@ class wnut_multiple_granularity(Dataset):
         ## return the length of dataset of any granularity (they must be all equal)
         for model_name, data_length in self.data_length_dict.items():
             print("model_name {model_name}, the length of dataset is : {length} ".format(model_name=model_name, length=data_length))
-        return self.data_length
+        return set(self.data_length.values())[0]
     
     def __getitem__(self, idx):
         ## should return {model_name : input_info}, where 
         ## input_info is input_ids, attn_mask, token_type_ids for each granularity. 
-        self.input_info = defaultdict(defaultdict)
-        self.input_info_test = defaultdict(defaultdict)
-        for model_name in self.args.model_names:
-            self.input_info[model_name]['input_ids'] = self.data[model_name]['train']['input_ids'] 
-            self.input_info[model_name]['attn_mask'] = self.data[model_name]['train']['attention_mask'] 
-            self.input_info[model_name]['token_type_ids'] = self.data[model_name]['train']['labels'] 
-            self.input_info_test[model_name]['input_ids'] = self.data[model_name]['test']['input_ids'] 
-            self.input_info_test[model_name]['attn_mask'] = self.data[model_name]['test']['attention_mask'] 
-            self.input_info_test[model_name]['token_type_ids'] = self.data[model_name]['test']['labels'] 
-        return self.input_info
+        # self.input_info = defaultdict(defaultdict)
+        # self.input_info_test = defaultdict(defaultdict)
+        # for model_name in self.args.model_names:
+        #     self.input_info[model_name]['input_ids'] = self.data[model_name]['train']['input_ids'] 
+        #     self.input_info[model_name]['attn_mask'] = self.data[model_name]['train']['attention_mask'] 
+        #     self.input_info[model_name]['token_type_ids'] = self.data[model_name]['train']['labels'] 
+        #     self.input_info_test[model_name]['input_ids'] = self.data[model_name]['test']['input_ids'] 
+        #     self.input_info_test[model_name]['attn_mask'] = self.data[model_name]['test']['attention_mask'] 
+        #     self.input_info_test[model_name]['token_type_ids'] = self.data[model_name]['test']['labels'] 
+        # return self.input_info
+        return self.data
         # raise NotImplementedError
 
 
@@ -88,7 +92,7 @@ class weighted_ensemble(BaseClassifier):
         for model_name in self.model_dict:
             model = self.model_dict[model_name]
             input_info = input_info_dict[model_name]
-            input_ids, attn_mask, token_type_ids = input_info["input_ids"], input_info["attn_mask"], input_info["token_type_ids"]
+            input_ids, attn_mask, token_type_ids = input_info["input_ids"], input_info["attention_mask"], input_info["ner_tags"]
             encoded = model(input_ids = input_ids, 
                             attention_mask = attn_mask,
                             token_type_ids = token_type_ids)
@@ -105,7 +109,38 @@ class weighted_ensemble(BaseClassifier):
         return logits ## [bs, seq_len, num_labels]
         
 
+class weighted_estimater(BaseEstimator):
+    def __init__(self, model, tokenizer, criterion=None, optimizer=None, scheduler=None, logger=None, writer=None, pred_thold=None, device='cpu', **kwargs):
+        super().__init__(model, tokenizer, criterion, optimizer, scheduler, logger, writer, pred_thold, device, **kwargs)
+        self.clip_grad_norm = 1
 
+    def step(self, data):
+        logits = self.model(
+            data['input_ids'].to(self.device, dtype=torch.long), 
+            data['attention_mask'].to(self.device, dtype=torch.long), 
+            data['ner_tags'].to(self.device, dtype=torch.long)
+        )
+        if self.mode in {'train', 'dev'}: 
+            # training or developmenting, ground true labels are provided
+            if self.mode == 'train': 
+                self.optimizer.zero_grad()
+            loss = self.criterion(logits, data['labels'].to(self.device, dtype=torch.float))
+            if self.mode == 'train': 
+                loss.backward()
+                nn.utils.clip_grad_norm_(self.model.parameters(), self.clip_grad_norm) # TODO: not equivalent to tf.clip_by_global_norm
+                self.optimizer.step()
+                if self.scheduler is not None: 
+                    self.scheduler.step()
+            return (
+                loss.detach().cpu().item(), 
+                torch.softmax(logits).detach().cpu().numpy(), 
+                data['labels'].numpy()
+            )
+        elif self.mode == 'test': 
+            # testing, no ground true label is provided
+            return None, torch.sigmoid(logits).detach().cpu().numpy(), None
+        else: 
+            raise ValueError(self.mode)
 
 
 
