eab5c737e9bcf986262c1b7e258dc954425029ca
samaritanhu <samaritanhu@gmail.com>
2022-10-12 21:42:47 -0400

    add dataset preprocess

--- utils/log.py
+++ utils/log.py
@@ -1,10 +1,18 @@
 import logging
+import os
+import sys
 
 logging.basicConfig(format = "%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s", level=logging.DEBUG)
 
 class Log:
-    def __init__(self, name) -> None:
+    def __init__(self, name, log_dir='log/') -> None:
         self._logger = logging.getLogger(name)
+
+        self.make_if_not_exists(log_dir)
+        self.file_handler = logging.FileHandler(
+            filename=os.path.join(log_dir, 'experiment.log')
+        )
+        self.stdout_handler = logging.StreamHandler(sys.stdout)
 
     def info(self, msg, *args, **kwargs):
         self._logger.info(msg, *args, **kwargs)
@@ -17,3 +25,7 @@ class Log:
     
     def error(self, msg, *args, **kwargs):
         self._logger.error(self, msg, *args, **kwargs)
+
+    def make_if_not_exists(self, new_dir): 
+        if not os.path.exists(new_dir): 
+            os.system('mkdir -p {}'.format(new_dir))
