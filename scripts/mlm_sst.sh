65468517396ba3ff0c959e5cc27d2fc2facc0ad4
samaritanhu <samaritanhu@gmail.com>
2023-02-27 21:39:00 -0800

    yay

--- scripts/mlm_sst.sh
+++ scripts/mlm_sst.sh
@@ -7,3 +7,4 @@ module load anaconda3
 conda activate py38
 # run the Unix 'echo' command
 python3 run.py --expr mlm_b --mlm_epochs 50 --n_epochs 100 --output_dir "/ocean/projects/cis230002p/xhu1/noisy-IE/"
+echo "done"
