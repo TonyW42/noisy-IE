2d86cf609fb60d7b8d663d66e707a95b6c9df82d
samaritanhu <samaritanhu@gmail.com>
2023-05-17 16:40:19 -0700

    add one more parameter for script

--- scripts/mlm_wnut_test.sh
+++ scripts/mlm_wnut_test.sh
@@ -9,5 +9,5 @@ module load anaconda3
 conda activate py310
 # run the Unix 'echo' command
 mkdir -p /ocean/projects/cis230002p/xhu1/noisy-IE/mlm_b
-python3 run.py --expr eval_wnut --mlm_epochs 1 --n_epochs 50 --num_att_layers 6 --test True --ckpt_name $ckpt_name --output_dir "/ocean/projects/cis230002p/xhu1/noisy-IE/mlm_b" > test-mlm-${ds}-$1.log
+python3 run.py --expr eval_wnut --mlm_epochs 1 --n_epochs 50 --num_att_layers $2 --test True --ckpt_name $ckpt_name --output_dir "/ocean/projects/cis230002p/xhu1/noisy-IE/mlm_b" > test-mlm-${ds}-$1.log
 echo "done"
