7dad80a1c0bf8f566bc06d13b2257ed9e4df7000
samaritanhu <samaritanhu@gmail.com>
2023-06-03 13:16:28 -0700

    add dataloader

--- evaluate_classifier.py
+++ evaluate_classifier.py
@@ -47,8 +47,8 @@ def fetch_classification_loader(dataset_name, char_tokenizer, word_tokenizer, ar
     test_dataset = classification_dataset(test_split, char_tokenizer, word_tokenizer, args)
 
     train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle = True)
-    val_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle = True)
-    test_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle = True)
+    val_loader = DataLoader(val_dataset, batch_size=args.train_batch_size, shuffle = True)
+    test_loader = DataLoader(test_dataset, batch_size=args.train_batch_size, shuffle = True)
 
     return train_loader, val_loader, test_loader
 
