57fce7233333b2a151634cbfe21113171b5a41d5
samaritanhu <samaritanhu@gmail.com>
2023-06-04 20:09:36 -0700

    run baseline

--- scripts/run_tweet_eval.py
+++ scripts/run_tweet_eval.py
@@ -18,9 +18,15 @@ dataset = [
 ]
 
 command_tweet = 'sbatch -p GPU-shared -t 8:00:00 -N 1 -n 1 -o eval_{dataset}_{num_att_layers}_ --gpus=v100-32:1 scripts/eval_script.sh {expr} {dataset} {num_labels} {num_att_layers}'
+command_baseline = 'sbatch -p GPU-shared -t 8:00:00 -N 1 -n 1 -o eval_{dataset}_ --gpus=v100-32:1 scripts/eval_script.sh {expr} {dataset} {num_labels}'
+# for dataset_name, labels in dataset:
+#     for att_layer_number in range(1,3):
+#         os.popen(command_tweet.format(dataset=dataset_name, 
+#                                       num_att_layers=att_layer_number, 
+#                                       expr=dataset_name, 
+#                                       num_labels=labels))
+
 for dataset_name, labels in dataset:
-    for att_layer_number in range(1,3):
-        os.popen(command_tweet.format(dataset=dataset_name, 
-                                      num_att_layers=att_layer_number, 
-                                      expr=dataset_name, 
-                                      num_labels=labels))
+    os.popen(command_baseline.format(dataset=dataset_name, 
+                                    expr=dataset_name, 
+                                    num_labels=labels))
