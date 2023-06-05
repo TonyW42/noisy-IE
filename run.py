1d4d28896cfc4fc334d504377a611d1586bca057
samaritanhu <samaritanhu@gmail.com>
2023-06-04 20:08:29 -0700

    add model type

--- run.py
+++ run.py
@@ -57,6 +57,7 @@ if __name__ == "__main__":
     parser.add_argument("--save", type=str, default="true")
     parser.add_argument("--layer_type", type=str, default="att")
     parser.add_argument("--mlm_epochs", type=int, default=100)
+    parser.add_argument("--model_type", type=str, default="base")
 
     ## NOTE: newly added
     parser.add_argument("--emb_size", type=int, default=768)
